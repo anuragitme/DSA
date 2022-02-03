47eb136337230aa6aa68fe9287964e40b8086fc0
anuragitme <anuragitme@gmail.com>
2022-02-03 23:37:22 +0530

    shortest distance btw 2 nodes.

---
+++ Tree/binary_tree/extra/shortest_distance_btw2nodes_bt.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct node{
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+node* LCA(node* root,int n1,int n2){
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data==n1 || root->data==n2){
+        return root;
+    }
+    node *left_lca=LCA(root->left,n1,n2);
+    node *right_lca=LCA(root->right,n1,n2);
+
+    if(left_lca!=NULL && right_lca!=NULL){
+        return root;
+    }
+    if(left_lca!=NULL){ // i.e. right is null
+        return left_lca;
+    }
+    return right_lca; // else left is null
+
+}
+int findDis(node *root,int k,int dist){
+    if(root==NULL){
+        return -1;//node not exists
+    }
+    if(root->data==k){
+        return dist;
+    }
+    int leftd=findDis(root->left,k,dist+1);
+    if(leftd!=-1){
+        return leftd;
+    }
+    return findDis(root->right,k,dist+1);
+
+
+}
+int shortDis(node *root,int n1,int n2){
+    node* lca=LCA(root,n1,n2);
+    
+    int d1=findDis(lca,n1,0);
+    int d2=findDis(lca,n2,0);
+
+    return d1+d2;
+}
+
+int main(){
+    node* root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+    root->left->left=new node(4);
+    root->left->right=new node(5);
+    root->right->right=new node(6);
+    root->right->left=new node(69);
+    root->right->right->left=new node(7);
+
+    /*              1
+                  /   \
+                2       3
+               / \     / \
+              4   5   69   6
+                         / \
+                        7   n
+    here,n==NULL
+    */  
+    node* root2=new node(1);
+    root2->left=new node(2);
+    root2->right=new node(3);
+    root2->left->left=new node(4);
+    root2->left->right=new node(5);
+    root2->left->left->left=new node(6);
+    /*              1                               1
+                  /   \                            / \
+                2       3                         2   3
+               / \     / \     --->              / \
+              4   5   n   n                     4  5
+            /  \ / \                           /
+           6   n n n                          6
+
+    here,n==NULL
+    */ 
+    
+    cout<<"shortest distance between 5 and 7 in first tree : "<<shortDis(root,5,7)<<endl;
+    cout<<"shortest distance between 4 and 6 in second tree : "<<shortDis(root2,4,6)<<endl;
+    return 0;
+} 
